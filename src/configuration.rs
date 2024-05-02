6ecd438bc34917cf476b717131f70092fcc04973
van <shenkefan3@gmail.com>
2024-05-02 08:59:30 +0800

    3.10.1

--- src/configuration.rs
+++ src/configuration.rs
@@ -20,8 +20,13 @@ impl DatabaseSettings {
             self.username, self.password, self.host, self.port, self.database_name
         )
     }
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgress://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
 }
-
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let settings = config::Config::builder()
         .add_source(config::File::new(
